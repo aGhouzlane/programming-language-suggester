0dca69c78dee42901b3cbe994bfdd5f939c95c71
aGhouzlane <ahmedghouzlane@gmail.com>
2021-04-05 11:21:25 -0400

    Add next and prev buttons and their logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $("form").submit(function (event) {
     event.preventDefault();
-    let yesSolve = document.getElementById("yesSolve").checked;
+    const yesSolve = document.getElementById("yesSolve").checked;
     const noSolve = document.getElementById("noSolve").checked;
     const maySolve = document.getElementById("maySolve").checked;
     const yesDesign = document.getElementById("yesDesign").checked;
@@ -16,13 +16,28 @@ $(document).ready(function () {
     const yesCoffee = document.getElementById("yesCoffee").checked;
     const noCoffee = document.getElementById("noCoffee").checked;
     const mayCoffee = document.getElementById("mayCoffee").checked;
+    const yesBall = document.getElementById("yesBall").checked;
+    const noBall = document.getElementById("noBall").checked;
+    const mayBall = document.getElementById("mayBall").checked;
+    const yesServer = document.getElementById("yesServer").checked;
+    const noServer = document.getElementById("noServer").checked;
+    const mayServer = document.getElementById("mayServer").checked;
+
 
     if (yesSolve && yesDesign && yesSeveral) {
+      $(".language").hide();
       $("form").hide();
       $("#html").show();
       $("#css").show();
       $("#javascript").show();
       $("#output").show();
+      $("#f-title").hide();
+    } else {
+      $(".language").hide();
+      $("form").hide();
+      $("#output").show();
+      $("#error").show();
+      $("#f-title").hide();
     }
   });
 
@@ -30,8 +45,32 @@ $(document).ready(function () {
     $(".unchecked").prop("checked", false);
     $("#output").hide();
     $("form").show();
+    $("#f-title").show();
   });
 
 
-});
+  $(".divs div").each(function (e) {
+    if (e != 0)
+      $(this).hide();
+  });
 
+  $("#next").click(function () {
+    if ($(".divs div:visible").next().length != 0)
+      $(".divs div:visible").next().show().prev().hide();
+    else {
+      $(".divs div:visible").hide();
+      $(".divs div:first").show();
+    }
+    return false;
+  });
+
+  $("#prev").click(function () {
+    if ($(".divs div:visible").prev().length != 0)
+      $(".divs div:visible").prev().show().next().hide();
+    else {
+      $(".divs div:visible").hide();
+      $(".divs div:last").show();
+    }
+    return false;
+  });
+});
